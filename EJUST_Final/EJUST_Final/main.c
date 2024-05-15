9224aa2ba2e3e567fd707b59c6a0c59e58d007f5
OmarAly03 <omar.aly@ejust.edu.eg>
2024-05-15 08:54:48 +0300

    Edited Main

--- EJUST_Final/EJUST_Final/main.c
+++ EJUST_Final/EJUST_Final/main.c
@@ -13,18 +13,38 @@
 #include "Keypad.h"
 
 unsigned char Local_u8Key;
+unsigned char arr[5];
+unsigned char *pass = "1234";
 
 int main(void){
 	LCD_Init();
 	KPD_Init();
 
     while (1){
-		// Get the pressed key
-		Local_u8Key = KPD_GetPressedKey();
-
-		if (Local_u8Key != '\0') {
-			// A key was pressed, print it to the serial port
+		for(unsigned char i = 0 ; i < 4 ; i++){
+			do{
+				Local_u8Key = KPD_GetPressedKey();
+			} while(Local_u8Key == '\0');
+			
+			arr[i] = Local_u8Key;
 			LCD_sendData(Local_u8Key);
+		}
+		arr[4] = '\0';
+		
+		unsigned char cond = 1;
+		
+		for(unsigned char i = 0 ; i < 5 ; i++){
+			if(arr[i] != pass[i]){
+				cond = 0;
+			}
+		}
+		
+		LCD_Clear();
+		if(cond){
+			LCD_sendString("Welcome");
+		}
+		else{
+			LCD_sendString("Incorrect");
 		}
 
 		_delay_ms(50); // Add a small delay to reduce CPU usage
